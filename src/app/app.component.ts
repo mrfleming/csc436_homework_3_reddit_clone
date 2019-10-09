b1779f6a5a0367101285f95a34d24846df596f9d
mrfleming <ericfleming8@gmail.com>
2019-10-09 14:14:32 -0500

    display static list of articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reddit-app';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Ben Simons hits a 3!!', 'https://espn.com/nba', 100),
+      new Article('Lakers destroy Warriors', 'https://espn.com/nba', 20),
+      new Article('Zion first game', 'https://espn.com/nba', 4),
+      new Article('Angular 8', 'https://angular.io', 8)
+    ];
+  }
 
   addArticle(title:HTMLInputElement, link:HTMLInputElement): boolean {
     console.log(`Adding content... title : ${title.value} and link : ${link.value}`);
